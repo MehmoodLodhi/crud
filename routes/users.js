0b2f8d772758d3f35ce74e3566c7604bab25ae9e
mehmoodlodhi <mehmoodlodhi3@gmail.com>
2023-06-02 02:47:17 +0500

    commented Email

--- routes/users.js
+++ routes/users.js
@@ -57,7 +57,7 @@ router.post("/addUser", async (req, res) => {
 
     const user_otp = new Otp({ user_id: user._id, value: otp });
     await user_otp.save();
-    await ourMail(user.email, otp);
+    // await ourMail(user.email, otp);
     await user.save();
     console.log(token);
     return res.status(200).send({ token });
