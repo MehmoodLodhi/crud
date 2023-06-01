d3f6a981bb5d4f82c87bc9bf31f24c11cc645621
mehmoodlodhi <mehmoodlodhi3@gmail.com>
2023-06-02 00:55:29 +0500

    things to consider but you didn't

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ async function db() {
 
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.log(err));
-  app.listen(3000, () => console.log("listening on port 3000"));
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 }
 
 db();
