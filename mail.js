95108324f48b58e71d364cdafb940100b38633eb
mehmoodlodhi <mehmoodlodhi3@gmail.com>
2023-06-06 23:44:51 +0500

    updated email settings aur kuch mere laiq?

--- mail.js
+++ mail.js
@@ -1,30 +1,10 @@
 var nodemailer = require("nodemailer");
 
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-  "299060008480-o9bh6a2524jh27oa8emhus8a7aa1rsei.apps.googleusercontent.com", //Client ID
-  "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", // Client Secret
-  "https://developers.google.com/oauthplayground" // Redirect URL
-);
-oauth2Client.setCredentials({
-  refresh_token:
-    "1//04OFm4hITiWrqCgYIARAAGAQSNwF-L9IrajJ-nna5TQndblgZVO-vQxQodxtfkNzagtPw4QXwyso_73kSVRAGNVw1LPDO8WnG6oE",
-});
-
-const accessToken = oauth2Client.getAccessToken();
 const smtpTransport = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    type: "OAuth2",
     user: "mehmoodlodhi3@gmail.com",
-    clientId:
-      "299060008480-o9bh6a2524jh27oa8emhus8a7aa1rsei.apps.googleusercontent.com",
-    clientSecret: "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",
-    refreshToken:
-      "1//04OFm4hITiWrqCgYIARAAGAQSNwF-L9IrajJ-nna5TQndblgZVO-vQxQodxtfkNzagtPw4QXwyso_73kSVRAGNVw1LPDO8WnG6oE",
-    accessToken: accessToken,
+    pass: "picscecsxaqjcbjd",
   },
 });
 async function ourMail(email, otp) {
@@ -40,4 +20,5 @@ async function ourMail(email, otp) {
     smtpTransport.close();
   });
 }
+
 module.exports.ourMail = ourMail;
